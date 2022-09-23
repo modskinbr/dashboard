5b182a2776440faca2b2f88d9f4db5e1770a2c78
modskinbr <lucassena021@gmail.com>
2022-09-23 13:46:40 -0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -3,8 +3,16 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-        localStorage.setItem("userdate", "Luckey");
-        localStorage.setItem("validate", "smodskins");
+// Login //
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username == "Luckey" && password == "smodskins") {
+        location.href = urlhref;
+        localStorage.setItem("userdate", username );
+        localStorage.setItem("validate", password );
         localStorage.setItem("data0", ID01.DiscordID);
         localStorage.setItem("data1", ID01.Nome);
         localStorage.setItem("data2", ID01.Imagem);
@@ -57,51 +65,12 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 		localStorage.setItem("dataq5", ID01.Quantidade5);
 		localStorage.setItem("dataq6", ID01.Quantidade6);
 		localStorage.setItem("dataq7", ID01.Quantidade7);
-
-// Login //
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username == "Luckey" && password == "smodskins") {
-        location.href = urlhref;
-        localStorage.setItem("userdate", username);
-        localStorage.setItem("validate", password);
-        localStorage.setItem("data1", ID01.Nome);
-        localStorage.setItem("data2", ID01.Imagem);
-        localStorage.setItem("data3", ID01.Pontos);
-        localStorage.setItem("data4", ID01.Rank);
-        localStorage.setItem("data5", ID01.Rank_Image);
-        localStorage.setItem("data6", ID01.Pontos_Miticos);
-        localStorage.setItem("data7", ID01.Elo);
-        localStorage.setItem("data8", ID01.Elo_Image);
-        localStorage.setItem("data9", ID01.Destaque);
-        localStorage.setItem("data10", ID01.Destaque_Image);
-        localStorage.setItem("data11", ID01.Fragmentos);
-        localStorage.setItem("data12", ID01.Fragmentos_Estelares);
-        localStorage.setItem("data13", ID01.Essencia_Mitica);
-        localStorage.setItem("data14", ID01.Moedas);
-        localStorage.setItem("data15", ID01.Parceria);
-        localStorage.setItem("data16", ID01.Cargo);
-        localStorage.setItem("data17", ID01.Cargo_Image);
-        localStorage.setItem("data18", ID01.Risco);
-        localStorage.setItem("data19", ID01.Imune);
-        localStorage.setItem("data20", ID01.Divulgacoes);
-        localStorage.setItem("data21", ID01.Colaboracoes);
-        localStorage.setItem("data22", ID01.Video);
-        localStorage.setItem("data23", ID01.Contribuicoes_Federativa);
-        localStorage.setItem("data24", ID01.Participacoes);
-        localStorage.setItem("data25", ID01.Bonus_Extra);
-        localStorage.setItem("data26", ID01.Contribuicoes_Tester);
-        localStorage.setItem("data27", ID01.Tarefas);
-        localStorage.setItem("data28", ID01.Verificacoes);
-        localStorage.setItem("data29", ID01.Progresso);
     }
     else if (username == "taikunxx3" && password == "taik9348") {
         location.href = urlhref;
-        localStorage.setItem("userdate", username);
-        localStorage.setItem("validate", password);
+        localStorage.setItem("userdate", username );
+        localStorage.setItem("validate", password );
+        localStorage.setItem("data0", ID02.DiscordID);
         localStorage.setItem("data1", ID02.Nome);
         localStorage.setItem("data2", ID02.Imagem);
         localStorage.setItem("data3", ID02.Pontos);
@@ -131,108 +100,28 @@ loginButton.addEventListener("click", (e) => {
         localStorage.setItem("data27", ID02.Tarefas);
         localStorage.setItem("data28", ID02.Verificacoes);
         localStorage.setItem("data29", ID02.Progresso);
-    }
-    else if (username == "Khyago" && password == "Khyagolol27") {
-        location.href = urlhref;
-        localStorage.setItem("userdate", username);
-        localStorage.setItem("validate", password);
-        localStorage.setItem("data1", ID03.Nome);
-        localStorage.setItem("data2", ID03.Imagem);
-        localStorage.setItem("data3", ID03.Pontos);
-        localStorage.setItem("data4", ID03.Rank);
-        localStorage.setItem("data5", ID03.Rank_Image);
-        localStorage.setItem("data6", ID03.Pontos_Miticos);
-        localStorage.setItem("data7", ID03.Elo);
-        localStorage.setItem("data8", ID03.Elo_Image);
-        localStorage.setItem("data9", ID03.Destaque);
-        localStorage.setItem("data10", ID03.Destaque_Image);
-        localStorage.setItem("data11", ID03.Fragmentos);
-        localStorage.setItem("data12", ID03.Fragmentos_Estelares);
-        localStorage.setItem("data13", ID03.Essencia_Mitica);
-        localStorage.setItem("data14", ID03.Moedas);
-        localStorage.setItem("data15", ID03.Parceria);
-        localStorage.setItem("data16", ID03.Cargo);
-        localStorage.setItem("data17", ID03.Cargo_Image);
-        localStorage.setItem("data18", ID03.Risco);
-        localStorage.setItem("data19", ID03.Imune);
-        localStorage.setItem("data20", ID03.Divulgacoes);
-        localStorage.setItem("data21", ID03.Colaboracoes);
-        localStorage.setItem("data22", ID03.Video);
-        localStorage.setItem("data23", ID03.Contribuicoes_Federativa);
-        localStorage.setItem("data24", ID03.Participacoes);
-        localStorage.setItem("data25", ID03.Bonus_Extra);
-        localStorage.setItem("data26", ID03.Contribuicoes_Tester);
-        localStorage.setItem("data27", ID03.Tarefas);
-        localStorage.setItem("data28", ID03.Verificacoes);
-        localStorage.setItem("data29", ID03.Progresso);
-    }
-    else if (username == "jake" && password == "bananinhadoroblox") {
-        location.href = urlhref;
-        localStorage.setItem("userdate", username);
-        localStorage.setItem("validate", password);
-        localStorage.setItem("data1", ID04.Nome);
-        localStorage.setItem("data2", ID04.Imagem);
-        localStorage.setItem("data3", ID04.Pontos);
-        localStorage.setItem("data4", ID04.Rank);
-        localStorage.setItem("data5", ID04.Rank_Image);
-        localStorage.setItem("data6", ID04.Pontos_Miticos);
-        localStorage.setItem("data7", ID04.Elo);
-        localStorage.setItem("data8", ID04.Elo_Image);
-        localStorage.setItem("data9", ID04.Destaque);
-        localStorage.setItem("data10", ID04.Destaque_Image);
-        localStorage.setItem("data11", ID04.Fragmentos);
-        localStorage.setItem("data12", ID04.Fragmentos_Estelares);
-        localStorage.setItem("data13", ID04.Essencia_Mitica);
-        localStorage.setItem("data14", ID04.Moedas);
-        localStorage.setItem("data15", ID04.Parceria);
-        localStorage.setItem("data16", ID04.Cargo);
-        localStorage.setItem("data17", ID04.Cargo_Image);
-        localStorage.setItem("data18", ID04.Risco);
-        localStorage.setItem("data19", ID04.Imune);
-        localStorage.setItem("data20", ID04.Divulgacoes);
-        localStorage.setItem("data21", ID04.Colaboracoes);
-        localStorage.setItem("data22", ID04.Video);
-        localStorage.setItem("data23", ID04.Contribuicoes_Federativa);
-        localStorage.setItem("data24", ID04.Participacoes);
-        localStorage.setItem("data25", ID04.Bonus_Extra);
-        localStorage.setItem("data26", ID04.Contribuicoes_Tester);
-        localStorage.setItem("data27", ID04.Tarefas);
-        localStorage.setItem("data28", ID04.Verificacoes);
-        localStorage.setItem("data29", ID04.Progresso);
-    }
-    else if (username == "imnotchinna" && password == "shintaro433") {
-        location.href = urlhref;
-        localStorage.setItem("userdate", username);
-        localStorage.setItem("validate", password);
-        localStorage.setItem("data1", ID05.Nome);
-        localStorage.setItem("data2", ID05.Imagem);
-        localStorage.setItem("data3", ID05.Pontos);
-        localStorage.setItem("data4", ID05.Rank);
-        localStorage.setItem("data5", ID05.Rank_Image);
-        localStorage.setItem("data6", ID05.Pontos_Miticos);
-        localStorage.setItem("data7", ID05.Elo);
-        localStorage.setItem("data8", ID05.Elo_Image);
-        localStorage.setItem("data9", ID05.Destaque);
-        localStorage.setItem("data10", ID05.Destaque_Image);
-        localStorage.setItem("data11", ID05.Fragmentos);
-        localStorage.setItem("data12", ID05.Fragmentos_Estelares);
-        localStorage.setItem("data13", ID05.Essencia_Mitica);
-        localStorage.setItem("data14", ID05.Moedas);
-        localStorage.setItem("data15", ID05.Parceria);
-        localStorage.setItem("data16", ID05.Cargo);
-        localStorage.setItem("data17", ID05.Cargo_Image);
-        localStorage.setItem("data18", ID05.Risco);
-        localStorage.setItem("data19", ID05.Imune);
-        localStorage.setItem("data20", ID05.Divulgacoes);
-        localStorage.setItem("data21", ID05.Colaboracoes);
-        localStorage.setItem("data22", ID05.Video);
-        localStorage.setItem("data23", ID05.Contribuicoes_Federativa);
-        localStorage.setItem("data24", ID05.Participacoes);
-        localStorage.setItem("data25", ID05.Bonus_Extra);
-        localStorage.setItem("data26", ID05.Contribuicoes_Tester);
-        localStorage.setItem("data27", ID05.Tarefas);
-        localStorage.setItem("data28", ID05.Verificacoes);
-        localStorage.setItem("data29", ID05.Progresso);
+        localStorage.setItem("data30", ID02.Nasceu);
+		localStorage.setItem("datap1", ID02.Obtido_recente);
+		localStorage.setItem("datap2", ID02.Obtido2);
+		localStorage.setItem("datap3", ID02.Obtido3);
+		localStorage.setItem("datap4", ID02.Obtido4);
+		localStorage.setItem("datap5", ID02.Obtido5);
+		localStorage.setItem("datap6", ID02.Obtido6);
+		localStorage.setItem("datap7", ID02.Obtido7);
+		localStorage.setItem("datab1", ID02.Bonus_recente);
+		localStorage.setItem("datab2", ID02.Bonus2);
+		localStorage.setItem("datab3", ID02.Bonus3);
+		localStorage.setItem("datab4", ID02.Bonus4);
+		localStorage.setItem("datab5", ID02.Bonus5);
+		localStorage.setItem("datab6", ID02.Bonus6);
+		localStorage.setItem("datab7", ID02.Bonus7);
+		localStorage.setItem("dataq1", ID02.Quantidade_recente);
+		localStorage.setItem("dataq2", ID02.Quantidade2);
+		localStorage.setItem("dataq3", ID02.Quantidade3);
+		localStorage.setItem("dataq4", ID02.Quantidade4);
+		localStorage.setItem("dataq5", ID02.Quantidade5);
+		localStorage.setItem("dataq6", ID02.Quantidade6);
+		localStorage.setItem("dataq7", ID02.Quantidade7);
     }
     else {
         loginErrorMsg.style.opacity = 1;
